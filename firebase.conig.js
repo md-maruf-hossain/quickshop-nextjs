0b1fce474d434a27fffe7fdca04a17cf9cc35b7f
md-maruf-hossain <marufhossain.6470@gmail.com>
2024-01-06 14:15:50 +0600

    app created and setup done

---
+++ firebase.conig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCD0-z8cI-kNApKz1xXLPEP7sfXA1gWJNQ",
+  authDomain: "quicshopbd.firebaseapp.com",
+  projectId: "quicshopbd",
+  storageBucket: "quicshopbd.appspot.com",
+  messagingSenderId: "663859776640",
+  appId: "1:663859776640:web:22f7939b62955297f2ea65"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
